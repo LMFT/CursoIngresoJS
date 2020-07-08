06b73f276964b393817c7db6c8ef57dec7a246b3
LMFT <lucasferrini1607@gmail.com>
2020-07-08 16:48:33 -0300

    .

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("ok");
+	alert("This is a test of the automated response system. Repeat. This was a test. Do not be alarmed!");
 }
 
